0dce11c321f1cd052d6df63e1ec9a50b2bd3c4c5
BombenlegerMoe <12684371+BombenlegerMoe@users.noreply.github.com>
2026-08-04 14:44:53 +0200

    Release 1.1.0
    - versionCode 3, versionName 1.1.0
    - fastlane changelog 3.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         targetSdk = 36
         // versionCode is what Android compares to decide an upgrade; versionName is only a label.
         // It must increase for an existing install to accept the new APK.
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
